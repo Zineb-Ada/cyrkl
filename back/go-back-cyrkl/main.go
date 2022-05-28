84d12e4db3233660c7c137df7c1500fbceb9ba69
Zineb-Ada <85489706+Zineb-Ada@users.noreply.github.com>
2022-05-28 19:53:54 +0200

    framework gin

---
+++ back/go-back-cyrkl/main.go
@@ -0,0 +1,24 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"github.com/gin-gonic/gin"
+// 	"net/http")
+
+// func main() {
+// 	fmt.Println("Hello World!")
+// }
+
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
