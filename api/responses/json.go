677efe859bb158ab3e5f0e5280f74d20daf550c1
Zineb-Ada <85489706+Zineb-Ada@users.noreply.github.com>
2023-01-20 16:20:02 +0100

    Update json.go

--- api/responses/json.go
+++ api/responses/json.go
@@ -6,8 +6,13 @@ import (
 	"net/http"
 )
 
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
+
 func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
 	w.WriteHeader(statusCode)
+	enableCors(&w)
 	err := json.NewEncoder(w).Encode(data)
 	if err != nil {
 		fmt.Fprintf(w, "%s", err.Error())
