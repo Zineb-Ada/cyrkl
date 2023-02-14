ce9fdfc9f72d5addc1e9ae45480738fa3a75c7f1
Zineb-Ada <85489706+Zineb-Ada@users.noreply.github.com>
2023-02-14 17:35:45 +0100

    Update routes.go

--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -12,7 +12,7 @@ func (s *Server) initializeRoutes() {
 
 	//Users routes
 	s.Router.HandleFunc("/users", middlewares.SetMiddlewareJSON(s.CreateUser)).Methods("POST")
-	s.Router.HandleFunc("/users", middlewares.SetMiddlewareJSON(s.GetUsers)).Methods("GET")
+	s.Router.HandleFunc("/usersG", middlewares.SetMiddlewareJSON(s.GetUsers)).Methods("GET")
 	s.Router.HandleFunc("/users/{id}", middlewares.SetMiddlewareJSON(s.GetUser)).Methods("GET")
 	s.Router.HandleFunc("/users/{id}", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareAuthentication(s.UpdateUser))).Methods("PUT")
 	s.Router.HandleFunc("/users/{id}", middlewares.SetMiddlewareAuthentication(s.DeleteUser)).Methods("DELETE")
@@ -30,5 +30,6 @@ func (s *Server) initializeRoutes() {
 	s.Router.HandleFunc("/invitationreceived", middlewares.SetMiddlewareJSON(s.GetRdInvitions)).Methods("GET")
 	s.Router.HandleFunc("/invitationreceived/{id}", middlewares.SetMiddlewareJSON(s.GetRdInvitation)).Methods("GET")
 	s.Router.HandleFunc("/invitationreceived/user/{user_receiver_id}", middlewares.SetMiddlewareJSON(s.GetUsersRdInvitationByUserID)).Methods("GET")
+	s.Router.HandleFunc("/invitationreceived/{id}", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareAuthentication(s.UpdateRdInvitation))).Methods("PUT")
 
 }
