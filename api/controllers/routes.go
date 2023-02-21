c3a9f46f820429bc548715ce38c35106de39c506
Zineb-Ada <85489706+Zineb-Ada@users.noreply.github.com>
2023-02-21 19:12:45 +0100

    add authentication

--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -14,7 +14,7 @@ func (s *Server) initializeRoutes() {
 	s.Router.HandleFunc("/user", middlewares.SetMiddlewareJSON(s.CreateUser)).Methods("POST")
 	s.Router.HandleFunc("/users", middlewares.SetMiddlewareJSON(s.GetUsers)).Methods("GET")
 	s.Router.HandleFunc("/user/{id}", middlewares.SetMiddlewareJSON(s.GetUser)).Methods("GET")
-	s.Router.HandleFunc("/updateuser/{id}", middlewares.SetMiddlewareJSON(s.UpdateUser)).Methods("POST")
+	s.Router.HandleFunc("/updateuser/{id}", middlewares.SetMiddlewareJSON(middlewares.SetMiddlewareAuthentication(s.UpdateUser))).Methods("POST")
 	s.Router.HandleFunc("/deleteuser/{id}", middlewares.SetMiddlewareAuthentication(s.DeleteUser)).Methods("DELETE")
 
 	// calendar routes
